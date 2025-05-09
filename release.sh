eecf266a7b51670f5a9169949aa32c722d5f62bc
clayton <hello@claytonc.dev>
2025-05-09 03:21:37 -0700

    add release process

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Set the target operating systems and architectures
+os_archs=("linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64")
+
+# Loop through the target OS and architectures
+for os_arch in "${os_archs[@]}"; do
+  os="${os_arch%%/*}"
+  arch="${os_arch##*/}"
+
+  # Set the GOOS and GOARCH environment variables
+  export GOOS="$os"
+  export GOARCH="$arch"
+
+  # Build the application
+  go build -o autoLightDark-$os-$arch
+
+  echo "Built for $os/$arch"
+done
